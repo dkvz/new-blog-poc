7522c6abfbcc40c38cf70250de5c82a7ea407b1e
DkVZ <bouc@clanz.be>
2019-10-26 16:55:45 +0200

    Working hero image swapper

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,28 @@
 const heroImages = document.querySelectorAll('.hero__img');
 const images = ['img/shrimp_plain1.svg', 'img/shrimp_plain2.svg'];
 
-heroImages.forEach((e, i) => {
-  fetch(images[i % heroImages.length])
-    .then(resp => {
-      if (resp.status >= 200 && resp.status < 300) {
-        resp.text().then(
-          data => {
-            const temp = document.createElement('div');
-            temp.innerHTML = data;
-            if (temp.firstElementChild.nodeName === 'svg') {
-              // Duplicate all attributes to the existing SVG.
-              
+function replaceHeroImages() {
+  heroImages.forEach((e, i) => {
+    fetch(images[i % heroImages.length])
+      .then(resp => {
+        if (resp.status >= 200 && resp.status < 300) {
+          resp.text().then(
+            data => {
+              const temp = document.createElement('div');
+              temp.innerHTML = data;
+              const svgEl = temp.firstElementChild;
+              if (temp.firstElementChild.nodeName === 'svg') {
+                // Duplicate the viewBox:
+                const newVb = temp.firstElementChild.getAttribute('viewBox');
+                if (newVb) e.setAttribute('viewBox', newVb);
+                e.innerHTML = svgEl.innerHTML;
+              }
             }
-          }
-        );
-      }
-    });
-});
+          );
+        }
+      });
+  });
+}
+
+// Delay the replacement just to see it in dev mode.
+setTimeout(replaceHeroImages, 4000);
